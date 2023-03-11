c82b282183d804f968b459eecf599f3db5b24a4b
Satyam Suman <findsatyam95@gmail.com>
2023-03-11 22:59:59 +0530

    Completed Dynamic array DS

--- src/ArrayListDS.java
+++ src/ArrayListDS.java
@@ -2,13 +2,25 @@ public class ArrayListDS {
 
     public static void main(String[] args) {
 
-        DynamicArray dynamicArray = new DynamicArray();
+        DynamicArray dynamicArray = new DynamicArray(5);
 
         dynamicArray.add("A");
         dynamicArray.add("B");
         dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
 
-        dynamicArray.insert(0, "X");
+//        dynamicArray.insert(0, "X");
+//        System.out.println(dynamicArray);
+//        dynamicArray.delete("A");
+
+        dynamicArray.delete("A");
+        dynamicArray.delete("C");
+        dynamicArray.delete("E");
+
+//        System.out.println(dynamicArray.size);
+//        System.out.println(dynamicArray.search("C"));
 
         System.out.println(dynamicArray);
         System.out.println(dynamicArray.size);
--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -34,19 +34,48 @@ public class DynamicArray {
     }
 
     public void delete(Object data) {
-
+        for (int i = 0; i < size; i++) {
+            if (array[i] == data) {
+                for (int j = 0; j < (size - i - 1); j++) {
+                    array[i + j] = array[i + j + 1];
+                }
+                array[size - 1] = null;
+                size--;
+                if (size <= (capacity / 3)) {
+                    shrink();
+                }
+                break;
+            }
+        }
     }
 
     public int search(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == data) {
+                return i;
+            }
+        }
         return -1;
     }
 
     private void grow() {
+        int newCapacity = capacity * 2;
+        Object[] newArray = new Object[newCapacity];
 
+        if (size >= 0) System.arraycopy(array, 0, newArray, 0, size);
+
+        capacity = newCapacity;
+        array = newArray;
     }
 
     private void shrink() {
+        int newCapacity = capacity / 2;
+        Object[] newArray = new Object[newCapacity];
 
+        if (size >= 0) System.arraycopy(array, 0, newArray, 0, size);
+
+        capacity = newCapacity;
+        array = newArray;
     }
 
     public boolean isEmpty() {
